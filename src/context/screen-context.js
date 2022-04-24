f3208e9f8ef750eadcc471895030b72d16f73c79
Luis Antonio <antonhy.romero98@gmail.com>
2022-04-24 00:38:13 -0500

    Calculadora funcional pero aun faltan funciones. Las operaciones basicas ya funcionan pero se podria agregar mas funcionalidad a la calculadora con algunos ligeros cambios.

--- src/context/screen-context.js
+++ src/context/screen-context.js
@@ -17,59 +17,87 @@ const initContent = {
   isLowerCero: true,
 };
 
+function operate(numero1, numero2, operator) {
+  const firstNumber = Number(numero1);
+  const secondNumber = Number(numero2);
+  let result;
+
+  switch (operator) {
+    case '+':
+      result =
+        Math.round((firstNumber + secondNumber + Number.EPSILON) * 100) / 100;
+      break;
+    case '-':
+      result =
+        Math.round((firstNumber - secondNumber + Number.EPSILON) * 100) / 100;
+      break;
+    case '*':
+      result =
+        Math.round((firstNumber * secondNumber + Number.EPSILON) * 100) / 100;
+      break;
+    case '/':
+      result =
+        Math.round((firstNumber / secondNumber + Number.EPSILON) * 100) / 100;
+      break;
+    default:
+      result = 'ERROR: Operation Invalid';
+  }
+
+  return result;
+}
+
 function screenContextReducer(state, action) {
   let lowerValue;
   let upperValue;
   switch (action.type) {
     case 'ADD_LOWER':
-      lowerValue = state.lowerScreen.concat(action.value);
-      return { ...state, lowerScreen: lowerValue };
+      if (state.isLowerCero || state.lowerScreen === '0') {
+        return {
+          ...state,
+          lowerScreen: action.value.toString(),
+          isLowerCero: false,
+        };
+      } else {
+        lowerValue = state.lowerScreen.concat(action.value);
+        return { ...state, lowerScreen: lowerValue };
+      }
     case 'REMOVE_LOWER':
       if (state.lowerScreen.length === 1) {
         lowerValue = '0';
+        return { ...state, lowerScreen: lowerValue, isLowerCero: true };
       } else {
-        lowerValue = state.lowerScreen.slice(0,-1);
+        lowerValue = state.lowerScreen.slice(0, -1);
+        return { ...state, lowerScreen: lowerValue };
       }
-
-      return { ...state, lowerScreen: lowerValue };
     case 'MOVE_TO_UPPER':
-      upperValue = state.lowerScreen.concat(action.operator);
-      return { lowerScreen: '0', upperScreen: upperValue, isLowerCero: true };
+      if (state.upperScreen === '') {
+        upperValue = state.lowerScreen.concat(action.operator);
+        return { lowerScreen: '0', upperScreen: upperValue, isLowerCero: true };
+      } else {
+        upperValue = state.upperScreen.split(' ');
+        lowerValue = state.lowerScreen;
+
+        const result = operate(upperValue[0], lowerValue, upperValue[1]);
+
+        return {
+          lowerScreen: result.toString(),
+          upperScreen: `${result}${action.operator}`,
+          isLowerCero: true,
+        };
+      }
     case 'OPERATE':
+      if (state.upperScreen.includes('=')) {
+        return { ...state };
+      }
       upperValue = state.upperScreen.split(' ');
       lowerValue = state.lowerScreen;
 
-      const firstNumber = Number(upperValue[0]);
-      const secondNumber = Number(lowerValue);
-      const operator = upperValue[1];
-
-      switch (operator) {
-        case '+':
-          lowerValue =
-            Math.round((firstNumber + secondNumber + Number.EPSILON) * 100) /
-            100;
-          break;
-        case '-':
-          lowerValue =
-            Math.round((firstNumber - secondNumber + Number.EPSILON) * 100) /
-            100;
-          break;
-        case '*':
-          lowerValue =
-            Math.round((firstNumber * secondNumber + Number.EPSILON) * 100) /
-            100;
-          break;
-        case '/':
-          lowerValue =
-            Math.round((firstNumber / secondNumber + Number.EPSILON) * 100) /
-            100;
-          break;
-      }
+      const result = operate(upperValue[0], lowerValue, upperValue[1]);
 
       return {
         ...state,
-        lowerScreen: lowerValue,
-        upperScreen: `${state.upperScreen}${secondNumber}`,
+        lowerScreen: result.toString(),
+        upperScreen: `${state.upperScreen}${lowerValue} = `,
       };
 
     default:
@@ -86,11 +114,12 @@ export function ScreenContextProvider(props) {
   const contextValue = {
     upperScreen: screensContent.upperScreen,
     lowerScreen: screensContent.lowerScreen,
-    addToLowerScreen: (value) => dispatch({type: 'ADD_LOWER', value}),
-    removeFromLowerScreen: () => dispatch({type: 'REMOVE_LOWER'}),
-    moveFromLowerToUpper: (operator) => dispatch({type: 'MOVE_TO_UPPER', operator}),
-    operateLowerWithUpper: () => dispatch({type: 'OPERATE'}),
-    clear: () => dispatch({type: 'CLEAR'}),
+    addToLowerScreen: (value) => dispatch({ type: 'ADD_LOWER', value }),
+    removeFromLowerScreen: () => dispatch({ type: 'REMOVE_LOWER' }),
+    moveFromLowerToUpper: (operator) =>
+      dispatch({ type: 'MOVE_TO_UPPER', operator }),
+    operateLowerWithUpper: () => dispatch({ type: 'OPERATE' }),
+    clear: () => dispatch({ type: 'CLEAR' }),
   };
 
   return (
