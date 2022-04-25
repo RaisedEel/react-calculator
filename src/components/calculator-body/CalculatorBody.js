634c410641fc48b7057ccc321d9af6191d3fff8a
Luis Antonio <antonhy.romero98@gmail.com>
2022-04-24 21:08:58 -0500

    Agregadas algunas clases y removido el componente ButtonPad. La razon es porque fue refactorado como CalculatorButton qque crea solo un boton en base a diferentes parametros. Version final por el momento.

--- src/components/calculator-body/CalculatorBody.js
+++ src/components/calculator-body/CalculatorBody.js
@@ -1,17 +1,45 @@
-import ButtonPad from './ButtonPad';
 import classes from './CalculatorBody.module.css';
+import CalculatorButton from './CalculatorButton';
 
-const NUMBER_PAD = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.', '+/-'];
-const OPERATOR_PAD = ['DEL', 'CLEAR', '*', '/', '+', '-', '='];
+//[7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.', '+/-'];
+const NUMBER_PAD = [
+  { content: '7', type: 'number'},
+  { content: '8', type: 'number'},
+  { content: '9', type: 'number'},
+  { content: '4', type: 'number'},
+  { content: '5', type: 'number'},
+  { content: '6', type: 'number'},
+  { content: '1', type: 'number'},
+  { content: '2', type: 'number'},
+  { content: '3', type: 'number'},
+  { content: '0', type: 'number'},
+  { content: '.', type: 'dot'},
+  { content: '+/-', type: 'negator'},
+];
+
+//['DEL', 'CLEAR', '*', '/', '+', '-', '='];
+const OPERATOR_PAD = [
+  { content: 'DEL', type: 'delete'},
+  { content: 'CLEAR', type: 'clear'},
+  { content: '*', type: 'operator'},
+  { content: '/', type: 'operator'},
+  { content: '+', type: 'operator'},
+  { content: '-', type: 'operator'},
+  { content: '=', type: 'equals'},
+];
 
 function CalculatorBody() {
   return (
     <div className={classes['calculator-body']}>
       <div className={classes['number-pad']}>
-        <ButtonPad key='numbers' buttons = {NUMBER_PAD} width = '31%'/>
+        {NUMBER_PAD.map((buttonConf) => (
+          <CalculatorButton key={buttonConf.content} {...buttonConf} />
+        ))}
       </div>
       <div className={classes['operator-pad']}>
-        <ButtonPad key='operators' buttons = {OPERATOR_PAD} width = '48%'/>
+        {OPERATOR_PAD.map((buttonConf) => (
+          <CalculatorButton key={buttonConf.content} {...buttonConf} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/calculator-body/CalculatorButton.js
@@ -0,0 +1,65 @@
+import { useContext } from 'react';
+
+import ScreenContext from '../../context/screen-context';
+import classes from './CalculatorButton.module.css';
+
+function CalculatorButton(props) {
+  const screenCtx = useContext(ScreenContext);
+
+  let buttonAction;
+  switch (props.type) {
+    case 'operator':
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'USE_OPERATOR',
+          operator: ` ${props.content} `,
+        });
+      break;
+    case 'delete':
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'REMOVE_DIGIT',
+        });
+      break;
+    case 'clear':
+      buttonAction = () => screenCtx.clear();
+      break;
+    case 'negator':
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'USE_NEGATION',
+        });
+      break;
+    case 'dot':
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'USE_DECIMALS',
+        });
+      break;
+    case 'equals':
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'USE_EQUALS',
+        });
+      break;
+    default:
+      // For numbers or unknown button inputs
+      buttonAction = () =>
+        screenCtx.dispatchAction({
+          type: 'ADD_DIGIT',
+          value: props.content,
+        });
+      break;
+  }
+
+  return (
+    <button
+      className={classes[props.type]}
+      onClick={buttonAction}
+    >
+      {props.content}
+    </button>
+  );
+}
+
+export default CalculatorButton;
